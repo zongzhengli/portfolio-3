b85cd5705deb56d088284e902f13188b540565cd
Zong Zheng Li <contact@zongzhengli.com>
2015-08-21 17:51:10 -0400

    Add 2048 bot
    Adds a page for the new project 2048 bot and fixes minor issues.

--- script/content.js
+++ script/content.js
@@ -36,7 +36,18 @@ var pages = [
         "links": [
             { "text": "DOWNLOAD", "url": "media/Absolute Zero.exe" },
             { "text": "GITHUB", "url": "https://github.com/zongzhengli/Absolute-Zero" }],
-        "description": "Absolute Zero is the product of endless hours of optimization and research. It is a C# chess engine written from scratch with an approximate playing strength of 2400 Elo. It defeats Windows' Chess Titans consistently, which was my goal when starting the project. It also defeats me consistently. Absolute Zero support UCI when started when argument -u."
+        "description": "Absolute Zero is the product of endless hours of optimization and research. It is a C# chess engine written from scratch with an approximate playing strength of 2237 Elo. It defeats Windows' Chess Titans consistently, which was my goal when starting the project. It also defeats me consistently. Absolute Zero support UCI when started when argument -u."
+    },
+    {
+        "title": "2048 BOT",
+        "id": "2048",
+        "bg": "images/bg_2048.png",
+        "fg": "images/fg_2048.png",
+        "hasFg": true,
+        "links": [
+            { "text": "VIDEO", "url": "https://www.youtube.com/watch?v=o6HGKy921YY" },
+            { "text": "GITHUB", "url": "https://github.com/zongzhengli/2048-Bot" }],
+        "description": "This is a bot for the web game 2048. It can readibly reach the 4096 tile while using only fractions of a second per move. It's written in JavaScript and is a lot of fun to watch."
     },
     {
         "title": "THREADS GAME",
@@ -44,7 +55,7 @@ var pages = [
         "bg": "images/bg_threads.png",
         "links": [
             { "text": "PLAY", "url": "media/Threads.html" }],
-        "description": "Threads is a puzzle Flash game written in ActionScript. It features a vector art style and an original physics implementation that can handle simple pendulum, box, and projectile physics. It is still in development, but there are 7 playable levels. Move with WASD and anchor threads with the mouse."
+        "description": "Threads is a puzzle/platformer game written that I developed with Flash in high school. It features a smooth camera and simple physics implementation. It was never completed but featjures 7 playable levels. Move with WASD and anchor ends of the threads with the cursor."
     },
     {
         "title": "LATTICE GRAPHICS",
@@ -52,7 +63,7 @@ var pages = [
         "bg": "images/bg_lattice.png",
         "links": [
             { "text": "DOWNLOAD", "url": "media/Lattice.dll" }],
-        "description": "I've often wanted to write applications with simple 3D graphics in C# so I created a library to make the process easy. Lattice is a 3D library for C#, featuring the ability to draw polygons with simple lighting and shading. It's currently primarily for personal use until I clean up some of the code and release documentation."
+        "description": "I've often wanted to explore ideas with simple 3D graphics so I created a library to make the process easy. Lattice is a 3D library for C#, featuring the ability to draw polygons with simple lighting and shading. It's currently primarily for personal use until I clean up some of the code and release documentation."
     },
     {
         "title": "FORCE-DIRECTED GRAPH",
@@ -69,6 +80,6 @@ var pages = [
         "bg": "images/bg_matrix.png",
         "links": [
             { "text": "DOWNLOAD", "url": "media/The Matrix Command Prompt.exe" }],
-        "description": "Sometimes I get silly ideas that I turn into afternoon projects. The Matrix Command Prompt is one of these fun concepts, implemented in Java and packaged into a Windows executable. Note it isn't 100% equivalent to the Windows command-line interpreter, so its use may even cause unexpected amounts of fun!"
+        "description": "Sometimes I get silly ideas that I turn into afternoon projects. The Matrix Command Prompt is one of these fun concepts, implemented in Java and packaged into a Windows executable. Note it isn't 100% equivalent to the Windows command-line interpreter, but it's still a lot of fun!"
     }
 ];
