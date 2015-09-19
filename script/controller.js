2ad88ceace8664c41afe88c3800a571925b190e1
Zong Zheng Li <contact@zongzhengli.com>
2015-09-19 15:43:39 -0700

    Allow html descriptions
    Adds feature to allow content descriptions to be html instead of just
    text.
    Fixes some improper bindings causing errors in the console.

--- script/controller.js
+++ script/controller.js
@@ -1,10 +1,14 @@
 var app = angular.module("mainApp", ['ngAnimate']);
 
-app.controller("controller", function ($scope) {
+app.controller("controller", [ '$scope', '$sce', function($scope, $sce) {
+    for (var i = 0 ; i < pages.length; i++) {
+    	pages[i].description = $sce.trustAsHtml(pages[i].description);
+    }
+
     $scope.pages = pages;
     $scope.activeId = pages[0].id;
 
     $scope.changePage = function (id) {
         $scope.activeId = id;
     }
-});
+}]);
